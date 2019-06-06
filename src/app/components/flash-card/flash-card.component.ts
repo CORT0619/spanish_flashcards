4a7efb025abc122437652aa561f69c91a3e8e1f9
ccmc-cwilliams <cwilliams@ccmcinc.com>
2019-06-06 13:47:26 -0400

    add styling to cards, add font

--- src/app/components/flash-card/flash-card.component.ts
+++ src/app/components/flash-card/flash-card.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'flash-card',
   templateUrl: './flash-card.component.html',
-  styleUrls: ['./flash-card.component.css']
+  styleUrls: ['./flash-card.component.scss']
 })
 export class FlashCardComponent implements OnInit {
   @Input() data: Array<Card>;
   currCard: Card;
+  answerVisible = false;
 
   constructor() {}
 
