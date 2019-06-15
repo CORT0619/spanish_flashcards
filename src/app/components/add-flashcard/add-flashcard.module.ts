040ce5044b932a0460cdf6a4ec317a81c3ff34ba
Crystal R. Williams <cortney0215@gmail.com>
2019-06-15 14:18:15 -0400

    add add-flashcard component

---
+++ src/app/components/add-flashcard/add-flashcard.module.ts
@@ -0,0 +1,16 @@
+import { AddFlashcardComponent } from './add-flashcard.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AddFlashcardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AddFlashcardComponent
+  ]
+})
+export class AddFlashcardModule { }
