164e29a78b1b16bf1e87e17569ce9534dbc82bfd
ccmc-cwilliams <cwilliams@ccmcinc.com>
2019-06-18 09:21:42 -0400

    bump up code coverage to 100%

--- src/app/components/cardholder/cardholder.component.spec.ts
+++ src/app/components/cardholder/cardholder.component.spec.ts
@@ -4,10 +4,18 @@ import { CardholderComponent } from './cardholder.component';
 import { NavArrowModule } from '@components/nav-arrow/nav-arrow.module';
 import { FlashCardModule } from '@components/flash-card/flash-card.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Card } from '@shared/models/card.model';
 
 describe('CardholderComponent', () => {
   let component: CardholderComponent;
   let fixture: ComponentFixture<CardholderComponent>;
+  const cards: Array<Card> = [
+    { question: 'Como is su pollo?', answer: 'How is your chicken?' },
+    {
+      question: 'El telefono es ocupado.',
+      answer: 'The telephone is occupied.'
+    }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,9 +28,18 @@ describe('CardholderComponent', () => {
     fixture = TestBed.createComponent(CardholderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.cards = cards;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#retrieveActiveCard', () => {
+    it('should set currElement and currCard', () => {
+      component.retrieveActiveCard(1);
+      expect(component.currElement).toEqual(1);
+      expect(component.currCard).toEqual(cards[1]);
+    });
   });
 });
